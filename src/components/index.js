0a08ca84f8aa8912524929062c6fd2954019395d
suraj kengar <surajkengar1212@gmail.com>
2026-02-17 18:29:55 +0530

    index.js file added

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+export * from "./common";
+export * from "./dashboard";
+export * from "./modals";
+export * from "./notes";
+export * from "./projects";
+export * from "./tasks";
+
+// Export auth components
+export { default as LoginForm } from "./auth/LoginForm";
+export { default as RegisterForm } from "./auth/RegisterForm";
